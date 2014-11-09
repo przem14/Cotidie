be5419b66e3b5e763d3977920a576db95276e6a1
smalec <kaczy2406@gmail.com>
2014-11-09 23:20:18 +0100

    Added PolishLanguagePack class

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  attr_reader :language
+  def initialize
+    require File.expand_path('../../../lib/languages/polish_language_pack', __FILE__)
+    @language = PolishLanguagePack.new
+  end
 end
---
+++ lib/languages/polish_language_pack.rb
@@ -0,0 +1,8 @@
+class PolishLanguagePack
+  attr_reader :sign_in, :sign_up
+
+  def initialize
+    @sign_in = "Zaloguj się"
+    @sign_up = "Zarejestruj się"
+  end
+end
