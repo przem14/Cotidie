00220e81bb16931824b1db1960e7e252734b0271
przem14 <przem.wawrzyniak@gmail.com>
2014-11-10 02:04:12 +0100

    Added users and updated PolishLanguagePack

--- lib/languages/polish_language_pack.rb
+++ lib/languages/polish_language_pack.rb
@@ -1,8 +1,32 @@
 class PolishLanguagePack
-  attr_reader :sign_in, :sign_up
+  attr_reader :sign_in, :sign_up, :sign_out,
+              :name, :surname, :personal_id, :role,
+              :password, :password_confirmation, :current_password,
+              :teacher, :tutor, :student, :admin,
+              :edit, :update,
+              :delete_account
 
   def initialize
-    @sign_in = "Zaloguj się"
-    @sign_up = "Zarejestruj się"
+    @sign_in  = 'Zaloguj się'
+    @sign_up  = 'Zarejestruj się'
+    @sign_out = 'Wyloguj się'
+
+    @name         = 'Imię'
+    @surname      = 'Nazwisko'
+    @personal_id  = 'PESEL'
+    @role         = 'Rodzaj uprawnień'
+    @password     = 'Hasło'
+    @password_confirmation  = 'Potwierdź hasło'
+    @current_password       = 'Aktualne hasło'
+
+    @teacher  = 'Nauczyciel'
+    @tutor    = 'Rodzic'
+    @student  = 'Uczeń'
+    @admin    = 'Administrator'
+
+    @edit   = 'Edytuj'
+    @update = 'Zapisz'
+
+    @delete_account = 'Usuń konto'
   end
 end
